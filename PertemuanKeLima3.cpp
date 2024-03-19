b53fa3c499cf2bd5c01b4df780292c4f6f424eba
rcslbaa <ifasmarani3125@gmail.com>
2024-03-19 19:24:42 +0700

    contoh 3

---
+++ PertemuanKeLima3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    cout << "Bilangan awal = " << bilangan << endl;
+    do
+    {
+        cout << "Perulangan DO..WHILE" << endl;
+        bilangan = rand() % 10;
+        cout << "Bilangannya = " << bilangan << endl;
+
+    } while (bilangan > 5);      
+}
