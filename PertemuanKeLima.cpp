599f9189b766b2942d9607ed90199460663b327d
rcslbaa <ifasmarani3125@gmail.com>
2024-03-19 17:57:39 +0700

    contoh 1

--- PertemuanKeLima.cpp
+++ PertemuanKeLima.cpp
@@ -1,20 +1,21 @@
-// PertemuanKeLima.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
 int main()
 {
-    std::cout << "Hello World!\n";
+    int i;
+    string nama[5];
+
+    for (i = 150; i > 100; i-=10)
+    {
+        cout << i << ". "<< "SELAMAT PUASA" << endl;
+
+    }
+    cout << "Nilai i terakhir = " << i << endl;
+
+    for (i = 0; i < 5; i++){
+        cout << "Masukkan nama ke-" << i << ":";
+        cin >> nama[i];
+    }
+
+    for (i = 0; i < 5; i++){
+        cout << "Nama ke-" << i << ": " << nama[i] << endl;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
